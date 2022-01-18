30500da7d77ccf4a9311cca4f9c845b776330b74
avcheban1988 <avcheban@gmail.com>
2022-01-18 16:11:09 +0300

    adjust package

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,3 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick({
-      speed: 300,
-      adaptiveHeight: true,
-      prevArrow: '<button type="button" class="slick-prev"><img scr="../icons/chevron-left-solid.png"></button>',
-      nextArrow: '<button type="button" class="slick-next"><img scr="../icons/chevron-right-solid.png"></button>'
-    });
-  });
+  $(".carousel__inner").owlCarousel();
+});
