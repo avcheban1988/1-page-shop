241bd159bc43a0dd29f50daeccc0398a175805cf
avcheban1988 <avcheban@gmail.com>
2022-01-19 01:53:04 +0300

    fixed gulpfile

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-  $(".carousel__inner").owlCarousel();
+  const slider = $("#slider").owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:5
+          }
+      }
+  });
 });
