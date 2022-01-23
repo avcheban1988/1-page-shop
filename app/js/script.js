55e7886132fcf4ce93325f5393c7111b59306430
avcheban1988 <avcheban@gmail.com>
2022-01-23 22:23:03 +0300

    сделал слайдер

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,29 @@
-$(document).ready(function(){
-  const slider = $("#slider").owlCarousel({
-      loop:true,
-      margin:10,
-      nav:true,
-      responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:3
-          },
-          1000:{
-              items:5
-          }
+var slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    controls: false,
+    responsive: {
+      769: {
+        edgePadding: 20,
+        gutter: 500,
+        nav: true,
+        items: 2,
+      },
+      320: {
+        gutter: 500
+      },
+      900: {
+        items: 1,
+        nav: false
       }
+    }
   });
-});
+
+document.querySelector('.prev').onclick = function () {    //На кнопку, с классом prev слайдер пойдет назад
+  slider.goTo('prev');
+};
+
+document.querySelector('.next').onclick = function () {    //На кнопку, с классом prev слайдер пойдет назад
+  slider.goTo('next');
+};
